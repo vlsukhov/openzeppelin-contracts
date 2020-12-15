c8f10c9cee3bbae47c93bc95e790cc11e91e6920
Vasiliy Sukov <75154976+vlsukhov@users.noreply.github.com>
2020-12-15 16:08:04 +0300

    Update synchronize-versions.js

--- scripts/release/synchronize-versions.js
+++ scripts/release/synchronize-versions.js
@@ -8,6 +8,18 @@ const cp = require('child_process');
 
 setVersion('contracts/package.json');
 
+extendEnvironment(env => {
+  const { contract } = env;
+
+  env.contract = function (name, body) {
+    // remove the default account from the accounts list used in tests, in order
+    // to protect tests against accidentally passing due to the contract
+    // deployer being used subsequently as function caller
+    contract(name, accounts => body(accounts.slice(1)));
+  };
+});
+
+
 function setVersion (file) {
   const json = JSON.parse(fs.readFileSync(file));
   json.version = process.env.npm_package_version;
